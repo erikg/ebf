bb9eba7a5efdcde0bd1dc0b1a8a590a6cd346bc3
erik <erik>
2005-08-30 02:42:22 +0000

    uh, rcs

--- bf2c.c
+++ bf2c.c
@@ -9,7 +9,7 @@ int main(int argc, char **argv) {
 	    case 'o':
 		break;
 	    case 'v':
-		printf("%s (bf2c) version $version$ ($header$)\n", *argv);
+		printf("%s (bf2c) version $Version$ ($Header: /mnt/fenris/usr/cvs/devel/brainfuck/Attic/bf2c.c,v 1.2 2005/08/30 02:42:22 erik Exp $)\n", *argv);
 		break;
 	    case 'h':
 		break;
