417c0f64fa1a27a8d7aa81b1673530031f2a7a0f
erik <erik>
2006-03-31 19:07:13 +0000

    ermf?

--- src/optimizer.c
+++ src/optimizer.c
@@ -13,18 +13,17 @@ compact (struct op_s *prog)
 	    case DEC:
 	    case NEXT:
 	    case PREV:
-	    if (prog->opcode == prog->next->opcode)
-	    {
-		struct op_s *tmp = prog->next;
+		if (prog->opcode == prog->next->opcode)
+		{
+		    struct op_s *tmp = prog->next;
 
-		prog->val += tmp->val;
-		prog->next = tmp->next;
-		free (tmp);
-		return compact (prog), prog;
-	    } else 
-		return compact (prog->next), prog;
-	    printf("I should never be seen\n");
-	    break;
+		    prog->val += tmp->val;
+		    prog->next = tmp->next;
+		    free (tmp);
+		    return compact (prog), prog;
+		} else 
+		    return compact (prog->next), prog;
+		printf("I should never be seen\n");
 	    case LOOP_START:
 		    prog->loop = compact(prog->loop);
 	default:
