54ce78dc04526bffcff1ac98a7729dd53860542d
erik <erik>
2006-09-15 23:39:05 +0000

    parser.h

--- src/bf2as.c
+++ src/bf2as.c
@@ -1,6 +1,6 @@
 
 /*
- * $Id: bf2as.c,v 1.3 2006/01/04 17:44:00 erik Exp $
+ * $Id: bf2as.c,v 1.4 2006/09/15 23:39:05 erik Exp $
  */
 
 #include <stdio.h>
@@ -10,51 +10,81 @@
 
 int labelcount = 0;
 
+static const char *head = "\
+	.text\n\
+	.global _start\n\
+kernel:\n\
+	int $0x80\n\
+	ret\n\
+_start:\n\
+	movl $heap,%%eax\n";
+static const char *tail = "\
+	pushl $0\n\
+	movl  $1, %%eax\n\
+	call  kernel\n\
+	leave\n\
+	ret\n\n\
+.data\n\
+	.local heap\n\
+	.comm heap,30000,32\n";
+
 void 
 x86(struct op_s *prog, char *oldlabel, char *label) {
-    char newlabel[] = "L0000";
+	char newlabel[] = "L####";
 
-    while(prog) {
+	while(prog) {
 	switch(prog->opcode){
 #define OP(o,c) case o: printf(c); break
-	OP(INC, "	inc (%%al)\t; +\n");
-	OP(DEC, "	dec (%%al)\t; -\n");
-	OP(NEXT, "	inc %%al\t\t; >\n");
-	OP(PREV, "	dec %%al\t; <\n");
-	OP(GET, "	*** GET\t\t; ,\n");
-	OP(PUT, "	*** PUT\t\t; .\n");
+	OP(INC,  "	incl (%%eax)	# +\n");
+	OP(DEC,  "	decl (%%eax)	# -\n");
+	OP(NEXT, "	incl  %%eax	# >\n");
+	OP(PREV, "	decl  %%eax	# <\n");
+	OP(GET,  "	*** GET\t\t# ,\n");
 #undef OP
+	case PUT:
+		printf("	pushl %%eax\n");
+		printf("	pushl $1\n");
+		printf("	pushl %%eax\n");
+		printf("	pushl $1\n");
+		printf("	movl  $4, %%eax\n");
+		printf("	call  kernel\n");
+		printf("	addl  $12, %%esp\n");
+		printf("	popl  %%eax\n");
+		break;
 	case LOOP_START:
-	    sprintf(newlabel, "L%04d", labelcount++);
-	    printf("%s:\t\t\t; [\n", label);
-	    printf("	cmp (%%al), 0\n");
-	    printf("	jnz %s\t\n", newlabel);
-	    x86(prog->loop, label, newlabel);
-	    break;
+		sprintf(newlabel, "L%04d", ++labelcount);
+		printf("%s:\t\t\t# [\n", label);
+		printf("	cmpb $0, (%%eax)\n");
+		printf("	jz %s\n", newlabel);
+		x86(prog->loop, label, newlabel);
+		break;
 	default:
-	    printf("Unknown opcode: %x \"%c\"\n", prog->opcode, prog->opcode);
-	    exit(-1);
+		printf("Unknown opcode: %x \"%c\"\n", prog->opcode, prog->opcode);
+		exit(-1);
 	}
 	prog = prog->next;
-    }
-    if(oldlabel){
-	printf("	jmp %s\t; ]\n", oldlabel);
-	printf("%s:\t\t\t\n", label);
-    }
+	}
+	if(oldlabel){
+	printf("	jmp %s	# ]\n", oldlabel);
+	printf("%s:			\n", label);
+	}
 }
 
 int
 main(int argc, char **argv) {
-    struct op_s *prog;
+	struct op_s *prog;
 
-    /* ensure a valid program filename */
-    if(argc<2 || (prog = parse (argv[1])) == NULL) {
-	printf("Usage\n\t%s <program.bf>\n", *argv);
+	/* ensure a valid program filename */
+	if(argc<2 || (prog = parse (argv[1])) == NULL) {
+	printf("Usage\n	%s <program.bf>\n", *argv);
 	return -1;
-    }
+	}
 
-    printf(".text\n_start:\n\tmovl $heap,%%eax\n");
-    x86(prog, NULL, "ROOT");
-    printf("\tleave\n\tret\n\n.data\n\t.local heap\n\t.comm heap,30000,32\n");
-    return 0;
+	printf("#\n# ");
+	prog_print(prog);
+	printf("#\n#\n#\n\n");
+	printf(head);
+	x86(prog, NULL, "ROOT");
+	printf(tail);
+	return 0;
 }
--- src/parser.h
+++ src/parser.h
@@ -2,12 +2,12 @@
 #define PARSER_H
 
 /*
- * $Id: parser.h,v 1.4 2006/01/03 22:37:50 erik Exp $
+ * $Id: parser.h,v 1.5 2006/09/15 23:39:05 erik Exp $
  */
 
 struct op_s {
 	char opcode;
-	int val;
+	int val;	    /* extra info for optimizations... repitiion count */
 	struct op_s *loop;
 	struct op_s *next;
 };
@@ -21,5 +21,6 @@ struct op_s *parse(char *filename);
 struct op_s *parsei(int fd);
 
 int prog_length(struct op_s *prog);
+void prog_print(struct op_s *prog);
 
 #endif
