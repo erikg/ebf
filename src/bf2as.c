2a4b47edd1edf4ca832274a449598ed3b891cfa3
erik <erik>
2006-09-16 00:16:40 +0000

    fixed looping recursion to be much cleaner and actually correct, w00t

--- src/bf2as.c
+++ src/bf2as.c
@@ -1,14 +1,12 @@
 
 /*
- * $Id: bf2as.c,v 1.4 2006/09/15 23:39:05 erik Exp $
+ * $Id: bf2as.c,v 1.5 2006/09/16 00:16:40 erik Exp $
  */
 
 #include <stdio.h>
 
 #include "lex.h"
 #include "parser.h"
-
-int labelcount = 0;
 
 static const char *head = "\
 	.text\n\
@@ -28,46 +26,51 @@ static const char *tail = "\
 	.local heap\n\
 	.comm heap,30000,32\n";
 
+int labelcount = 0;
+
 void 
-x86(struct op_s *prog, char *oldlabel, char *label) {
-	char newlabel[] = "L####";
+x86(struct op_s *prog) {
+	char label[] = "L####", endlabel[] = "L####";
+	int labelval = labelcount;
 
 	while(prog) {
-	switch(prog->opcode){
+		switch(prog->opcode){
 #define OP(o,c) case o: printf(c); break
-	OP(INC,  "	incl (%%eax)	# +\n");
-	OP(DEC,  "	decl (%%eax)	# -\n");
-	OP(NEXT, "	incl  %%eax	# >\n");
-	OP(PREV, "	decl  %%eax	# <\n");
-	OP(GET,  "	*** GET\t\t# ,\n");
+		OP(INC,  "	incl (%%eax)	# +\n");
+		OP(DEC,  "	decl (%%eax)	# -\n");
+		OP(NEXT, "	incl  %%eax	# >\n");
+		OP(PREV, "	decl  %%eax	# <\n");
+		OP(GET,  "	*** GET\t\t# ,\n");
 #undef OP
-	case PUT:
-		printf("	pushl %%eax\n");
-		printf("	pushl $1\n");
-		printf("	pushl %%eax\n");
-		printf("	pushl $1\n");
-		printf("	movl  $4, %%eax\n");
-		printf("	call  kernel\n");
-		printf("	addl  $12, %%esp\n");
-		printf("	popl  %%eax\n");
-		break;
-	case LOOP_START:
-		sprintf(newlabel, "L%04d", ++labelcount);
-		printf("%s:\t\t\t# [\n", label);
-		printf("	cmpb $0, (%%eax)\n");
-		printf("	jz %s\n", newlabel);
-		x86(prog->loop, label, newlabel);
-		break;
-	default:
-		printf("Unknown opcode: %x \"%c\"\n", prog->opcode, prog->opcode);
-		exit(-1);
+		case PUT:
+			printf("	pushl %%eax\n");
+			printf("	pushl $1\n");
+			printf("	pushl %%eax\n");
+			printf("	pushl $1\n");
+			printf("	movl  $4, %%eax\n");
+			printf("	call  kernel\n");
+			printf("	addl  $12, %%esp\n");
+			printf("	popl  %%eax\n");
+			break;
+		case LOOP_START:
+			labelval = labelcount;
+			++labelcount;
+			sprintf(label, "B%04d", labelval);
+			sprintf(endlabel, "E%04d", labelval);
+			printf("%s:\t\t\t# [\n", label);
+			printf("	cmpb $0, (%%eax)\n");
+			printf("	jz %s\n", endlabel);
+			x86(prog->loop);
+			printf("	jmp %s	# ]\n", label);
+			printf("%s:			\n", endlabel);
+			break;
+		default:
+			printf("Unknown opcode: %x \"%c\"\n", prog->opcode, prog->opcode);
+			exit(-1);
+		}
+		prog = prog->next;
 	}
-	prog = prog->next;
-	}
-	if(oldlabel){
-	printf("	jmp %s	# ]\n", oldlabel);
-	printf("%s:			\n", label);
-	}
+	return;
 }
 
 int
@@ -84,7 +87,7 @@ main(int argc, char **argv) {
 	prog_print(prog);
 	printf("#\n#\n#\n\n");
 	printf(head);
-	x86(prog, NULL, "ROOT");
+	x86(prog);
 	printf(tail);
 	return 0;
 }
