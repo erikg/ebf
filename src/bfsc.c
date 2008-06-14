5a4265b089baf0681ce8ab0d539319398769cb54
erik <erik>
2008-06-14 13:22:28 +0000

    comment stripper (reformatter) added

---
+++ src/bfsc.c
@@ -0,0 +1,62 @@
+/******************************************************************************
+ * Copyright (C) 2005-2008 Erik Greenwald. All rights reserved.		      *
+ * 									      *
+ * Redistribution and use in source and binary forms, with or without	      *
+ * modification, are permitted provided that the following conditions	      *
+ * are met:								      *
+ * 1. Redistributions of source code must retain the above copyright	      *
+ *    notice, this list of conditions and the following disclaimer.	      *
+ * 2. Redistributions in binary form must reproduce the above copyright	      *
+ *    notice, this list of conditions and the following disclaimer in the     *
+ *    documentation and/or other materials provided with the distribution.    *
+ * 									      *
+ * THIS SOFTWARE IS PROVIDED BY AUTHOR AND CONTRIBUTORS ``AS IS'' AND	      *
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE      *
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE *
+ * ARE DISCLAIMED.  IN NO EVENT SHALL AUTHOR OR CONTRIBUTORS BE LIABLE	      *
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL *
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS    *
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)      *
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT *
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  *
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF     *
+ * SUCH DAMAGE.								      *
+ *****************************************************************************/
+/*
+ * $Id: bfsc.c,v 1.1 2008/06/14 13:22:28 erik Exp $
+ */
+
+#include <stdio.h>
+
+#include "parser.h"
+
+void
+print_prog(struct op_s *prog) {
+    int i;
+    while(prog) {
+	if(prog->opcode == '[') {
+	    printf("[");
+	    print_prog(prog->loop);
+	    printf("]");
+	} else
+	    for(i=0;i<prog->val;i++)
+		printf("%c", prog->opcode);
+	prog = prog->next;
+    }
+}
+
+int
+main(int argc, char **argv) {
+    struct op_s *prog;
+
+    /* ensure a valid program filename */
+    if(argc<2 || (prog = parse (argv[1])) == NULL) {
+	printf("Usage\n\t%s <program.bf>\n", *argv);
+	return -1;
+    }
+
+    print_prog(prog);
+    printf("\n");
+
+    return 0;
+}
